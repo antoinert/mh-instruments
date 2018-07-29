67255590270f8cae11662f2eb54239b8e13a0e79
Anton Rautio <anton.rautio@smartly.io>
2018-07-29 16:48:44 +0300

    Start hinnasto

--- src/components/app.js
+++ src/components/app.js
@@ -48,6 +48,84 @@ export const INSTRUMENTS = {
   }
 };
 
+export const PRICES = {
+  "perushuolto": {
+    "id": "perushuolto",
+    "price": "60.00",
+    "features": ['floyd rose tyylinen + 15€', 'yksittäisen nauhan hionta, pyöristys ja kiillotus 30€ + 10€ lisää / nauha']
+  },
+  "yläsatulan valmistus": {
+    "id": "yläsatulan valmistus",
+    "price": "60.00",
+    "features": ["sisältää materiaalikulut"]
+  },
+  "alasatulan valmistus": {
+    "id": "alasatulan valmistus",
+    "price": "70.00",
+    "features": ["kompensoitu / intonoitu", "sisältää materiaalikulut"]
+  },
+  "nauhojen hionta": {
+    "id": "nauhojen hionta",
+    "price": "110.00",
+    "features": ["sisältää nauhojen hionnan, pyöristyksen ja kiillotuksen sekä perushuollon"]
+  },
+  "nauhojen vaihto (pulttikaulainen)": {
+    "id": "nauhojen vaihto (pulttikaulainen)",
+    "price": "210.00",
+    "features": ["stainless steel + 80€", "vaahteraotelautainen / liimakaulainen + 40€", "sisältää nauhojen hionnan + nauhamateriaalit"]
+  },
+  "murtunut lapa": {
+    "id": "murtunut lapa",
+    "price": "280.00",
+    "features": ["lavan liimaus ja kolojen paikkaus", "sis. kaulan osittainen lakkaus"]
+  },
+  "kytkimen, potentiometrin tai jakin vaihto": {
+    "id": "kytkimen, potentiometrin tai jakin vaihto",
+    "price": "35.00",
+    "features": ["+ materiaalit €"]
+  },
+  "mikrofonin vaihto": {
+    "id": "mikrofonin vaihto",
+    "price": "55.00",
+    "features": ["lisämikrofoni +15€"]
+  },
+  "mikrofonin uudelleenkäämintä": {
+    "id": "mikrofonin uudelleenkäämintä",
+    "price": "90.00",
+    "features": []
+  },
+  "häiriösuojaus": {
+    "id": "häiriösuojaus",
+    "price": "65.00",
+    "features": ["mikrofoni- ja elektroniikkapoteroiden, sekä tarvittaessa pleksin sisäpuolen vuoraus kupariteipillä"]
+  },
+  "CAD-piirtäminen": {
+    "id": "CAD-piirtäminen",
+    "price": "alk. 60.00",
+    "features": ["valmiin piirustuksesi tai suunnitelmasi virtualisoiminen / muuttaminen .dwg ja .dxf muotoon"]
+  },
+  "pulttikaulainen RHINO -kitara tai NGIN -basso": {
+    "id": "pulttikaulainen RHINO -kitara tai NGIN -basso",
+    "price": "alk 1950.00",
+    "features": []
+  },
+  "liimakaulainen Perfect 6 -kitara kaarevalla kannella": {
+    "id": "liimakaulainen Perfect 6 -kitara kaarevalla kannella",
+    "price": "alk. 2069.00",
+    "features": []
+  },
+  "lavaton Matinheikki Instruments sähkökitara tai -basso": {
+    "id": "lavaton Matinheikki Instruments sähkökitara tai -basso",
+    "price": "alk. 2400.00",
+    "features": []
+  },
+  "akustinen teräskielinen Matinheikki Instruments kitara": {
+    "id": "akustinen teräskielinen Matinheikki Instruments kitara",
+    "price": "alk. 2600.00",
+    "features": []
+  }
+}
+
 export default class App extends Component {
   render() {
     return (
--- src/components/hinnasto.js
+++ src/components/hinnasto.js
@@ -1,9 +1,71 @@
 import React, { Component } from 'react';
+import { PRICES } from './app';
+import _ from 'lodash';
 
 export default class Hinnasto extends Component {
+  renderPrices() {
+    return _.map(PRICES, price => {
+      return (
+        <tr>
+          <td style={{border: "1px solid #dddddd", paddingLeft: 5}}>
+            <div><b>{price.id}</b></div>
+            <div>
+              <ul>
+                {price.features.map(feature => {
+                  return <li>{feature}</li>
+                })}
+              </ul>
+            </div>
+          </td>
+          <td style={{border: "1px solid #dddddd", textAlign: "center"}}>
+            {price.price}
+          </td>
+        </tr>
+      )
+    })
+  }
+
+  renderDescription() {
+    return (
+      <div>
+        <h4>Hinnat sisältävät alv 24%</h4>
+        <h4><b>Perushuolto sisältää</b></h4>
+        <ul>
+          <li>kaularaudan säätö</li>
+          <li>otelaudan puhdistus ja öljyäminen</li>
+          <li>tallan intonaation sääto ja tarkistus</li>
+          <li>kielten korkeuden säätö asiakkaan toiveen mukaan</li>
+          <li>satulaurien syvennys tarvittaessa</li>
+          <li>elektroniikan toimivuuden tarkistus</li>
+          <li>kielten vaihto (ei sis. kieliä)</li>
+          <li>mikrofonien korkeuden säätö ja tasapainoisuuden tarkistus</li>
+          <li>viritys ja koesoitto</li>
+        </ul>
+      </div>
+    );
+  }
+
   render() {
     return (
-      <p>Hinnasto</p>
+      <div class="container">
+        <div class="row">
+          <div class="col-md-1">
+          </div>
+          <div class="col-md-5">
+            <table>
+              <tr>
+                <th>Hinnasto</th>
+              </tr>
+              {this.renderPrices()}
+            </table>
+          </div>
+          <div class="col-md-5">
+            <ul>
+              {this.renderDescription()}
+            </ul>
+          </div>
+        </div>
+      </div>
     );
   }
 }
